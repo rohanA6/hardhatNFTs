2ef30caf9c02b156bcd338230956008047e73ba8
Rohan <rohanahire651@gmail.com>
2022-07-30 00:27:33 +0530

    error fixed in contracts

--- contracts/BasicNFT.sol
+++ contracts/BasicNFT.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNFT is ERC721 {
     
-    uint256 private s_tokenCounter,
+    uint256 private s_tokenCounter;
 
     string public constant TOKEN_URI = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
@@ -16,7 +16,7 @@ contract BasicNFT is ERC721 {
 
     function mintNft() public returns(uint256) {
         _safeMint(msg.sender,s_tokenCounter );
-        s_tokenCounter + +;
+        s_tokenCounter++;
         return s_tokenCounter;
     }
 
